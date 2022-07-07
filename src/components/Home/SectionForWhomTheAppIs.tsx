5ad13e91428fa84df0b18068bc88eef9f1274327
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-07-07 13:27:07 +0200

    SectionForWhomTheAppIs on the HomePage was done.

--- src/components/Home/SectionForWhomTheAppIs.tsx
+++ src/components/Home/SectionForWhomTheAppIs.tsx
@@ -4,6 +4,16 @@ import '../../styles/SectionForWhomTheAppIs.css';
 
 export const SectionForWhomTheAppIs = () => {
     return (
-        <div className="section-info-1"/>
+        <div className="section-for-whom">
+            <div className="for-whom-info">
+                <h2>Dla kogo?</h2>
+                <div>
+                    <p>Aplikacja SoBitem została przygotowana dla dwóch grup osób:</p>
+                    <p>1. Sprzedających</p>
+                    <p>2. Kupujących</p>
+                </div>
+            </div>
+            <div className="for-whom-picture"/>
+        </div>
     )
 }
